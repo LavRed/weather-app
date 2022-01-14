41b97c82aff1768cd8ba4ec66d3bfb7046a4b67f
LavRed <74660422+LavRed@users.noreply.github.com>
2022-01-14 12:56:46 -0500

    Initial commit

---
+++ src/Weather.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Audio } from "react-loader-spinner";
+
+
+export default function Weather() {
+    return (
+        <div>
+        <h1>
+        Hello from Weather
+        </h1>  
+        <Audio
+        height="100"
+        width="100"
+        color='grey'
+        />
+        </div> 
+    )
+}
